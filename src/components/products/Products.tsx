71384276392311b838324efa9dc232b12e58ac7e
Flow-flex-the-complex <fth29@hi.is>
2020-04-12 16:02:42 +0000

    betra virkni

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -4,23 +4,32 @@ import { IProduct } from '../../api/types';
 
 import './Products.scss';
 
-export default function Products(props: { productList: IProduct[], onClick?: any }) {
+export default function Products(props: {
+  productList: IProduct[];
+  onClick?: any;
+}) {
   const { productList, onClick } = props;
 
   return (
     <Fragment>
       {productList.map((product, i) => {
-
         return (
           <div key={i} className="products__col">
             <div className="products__product">
-              <Link onClick={() => onClick(product.id)} className="products__link" to={`/product/${product.id}`}>
+              <Link
+                onClick={() => onClick(product.id)}
+                className="products__link"
+                to={`/product/${product.id}`}
+              >
                 <div className="products__imgcont">
                   <img className="products__image" src={product.image} />
                 </div>
                 <div className="products__info">
                   <div className="products__descr">
-                    <h3 className="products__title">{product.title}</h3>
+                    <h3
+                      className="products__title"
+                      dangerouslySetInnerHTML={{ __html: product.title }}
+                    ></h3>
                     <p>{product.category_title}</p>
                   </div>
                   <p className="products__price">{product.price} kr.-</p>
